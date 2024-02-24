c68971639a6e9405ba7f8416c146a28017c32aa5
uelionobre <uelionobre@hotmail.com>
2024-02-23 22:09:04 -0300

    Adiciona variaveis de ambiente, helmet e altera conteúdo da rota principal

--- src/app.js
+++ src/app.js
@@ -1,18 +1,21 @@
-const express = require('express')
-const cors = require('cors')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 
-const app = express()
+const PORT = process.env.API_PORT || 3000;
 
-app.use(express.json())
-app.use(cors())
+const app = express();
 
-app.get('/', (req, res) => res.json(
-  { a: 1, b: 2, c: 3 }
-))
+app.use(helmet());
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => res.json({ info: `API running on port: ${PORT}` }));
 
 app.post('/', (req, res) => {
-  const { body } = req
-  return res.json({ info: 'Body modificado', body })
-})
+  const { body } = req;
+  return res.json({ info: 'Body modificado', body });
+});
 
 module.exports = app;
