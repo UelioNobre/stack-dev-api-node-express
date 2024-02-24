0dd2d14b417b0efd52b0e61d04cddab0758cee2a
uelionobre <uelionobre@hotmail.com>
2024-02-24 17:59:56 -0300

    Adiciona rota posts a aplicação

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express')
 const homeRouter = require('./home.router')
+const postsRouter = require('./posts.router')
 
 const router = Router({ caseSensitive: true });
 
 router.use('/', homeRouter)
+router.use('/posts', postsRouter)
 
 module.exports = router;
