b16271d1d573003347e2920423e91078217539cb
uelionobre <uelionobre@hotmail.com>
2024-02-27 23:38:15 -0300

    Adiciona mock de usuario

--- tests/mocks.js
+++ tests/mocks.js
@@ -3,9 +3,16 @@ const postMockWithId = { id: 1, ...postMock }
 const postCreatedMockExpected = { message: postMockWithId, statusCode: 201 }
 const postCreatedJSONMockExpected = { post: postMockWithId }
 
+const userMock = {
+  name: 'Uélio Nobre',
+  email: 'uelio@nobre.com',
+  password: 'password'
+}
+
 module.exports = {
   postMock,
   postMockWithId,
   postCreatedMockExpected,
-  postCreatedJSONMockExpected
+  postCreatedJSONMockExpected,
+  userMock
 }
