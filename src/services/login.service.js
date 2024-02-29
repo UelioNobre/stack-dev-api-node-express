0fdde1c1d3cd3ae2913780894b8405b8e96511c2
uelionobre <uelionobre@hotmail.com>
2024-02-29 14:27:34 -0300

    Corrige código com eslint

--- src/services/login.service.js
+++ src/services/login.service.js
@@ -1,26 +1,26 @@
-require('dotenv').config()
-const userService = require('./users.service')
-const jwt = require('jsonwebtoken')
+require("dotenv").config();
+const userService = require("./users.service");
+const jwt = require("jsonwebtoken");
 
 
 function generateToken(payload) {
-  const secret = process.env.JWT_SECRET
-  const algorithm = 'HS256'
-  const token = jwt.sign(payload, secret, { algorithm })
-  return { statusCode: 200, message: { token } }
+  const secret = process.env.JWT_SECRET;
+  const algorithm = "HS256";
+  const token = jwt.sign(payload, secret, { algorithm });
+  return { statusCode: 200, message: { token } };
 }
 
 function verifyPassword(user, password) {
   if (user.password !== password)
-    throw new Error('User not found', { cause: { statusCode: 404 } })
+    throw new Error("User not found", { cause: { statusCode: 404 } });
 
-  const { id, name, email } = user
+  const { id, name, email } = user;
   return { id, name, email };
 }
 
 async function findByEmail(email) {
-  const request = await userService.findByEmail(email)
-  return request
+  const request = await userService.findByEmail(email);
+  return request;
 }
 
 
@@ -28,4 +28,4 @@ module.exports = {
   findByEmail,
   verifyPassword,
   generateToken
-}
+};
